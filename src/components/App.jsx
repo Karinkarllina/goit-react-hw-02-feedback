657df367818e59a172e69f8beecd7274a8d1706f
Karinkarllina <karinkhaliullina@gmail.com>
2023-05-08 18:02:37 +0300

    Початок роботи

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  addFeedbackGood = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }))
+    
+  }
+  addFeedbackBad = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }))
+    
+  }
+
+  addFeedbackNeutral = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }))
+    
+  }
+
+  countTotalFeedback = ({ good, neutral, bad }) => good + neutral + bad;
+  
+  countPositiveFeedbackPercentage = ({ good }) =>
+    this.countTotalFeedback(this.state) ?
+      Math.round((good * 100) / this.countTotalFeedback(this.state)) :
+      0;
+
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <section>
+        <h1>Please leave feedback</h1>
+        <div>
+          <button type='button'onClick={this.addFeedbackGood}>Good</button>
+          <button type='button'onClick={this.addFeedbackNeutral}>Neutral</button>
+          <button type='button'onClick={this.addFeedbackBad}>Bad</button>
+        </div>
+        <p>Statistics</p>
+        <div>
+          <p>Good: <span>{good}</span></p>
+          <p>Neutral: <span>{neutral}</span></p>
+          <p>Bad: <span>{bad}</span></p>
+          <p>Total: <span>{this.countTotalFeedback(this.state)}</span></p>
+          <p>Positive Feedback: <span>{this.countPositiveFeedbackPercentage(this.state)}%</span></p>
+        </div>
+      </section>
+    );
+  }
+}
+
